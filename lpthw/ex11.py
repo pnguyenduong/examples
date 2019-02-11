0c3d81be978b97688ac4c4d8db9f1ef344a73eac
Phong Nguyen <pnguyenduong@gmail.com>
2019-02-11 05:35:13 -0600

    Exercise 11

---
+++ lpthw/ex11.py
@@ -0,0 +1,18 @@
+'''
+	Asking Questions
+'''
+
+'''
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+'''
+
+age = int(input("How old are you? "))
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
