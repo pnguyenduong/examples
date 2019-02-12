102657d51fa7091e19e8587717e9622d703747e0
Phong Nguyen <pnguyenduong@gmail.com>
2019-02-11 23:07:52 -0600

    Exercise 21

---
+++ lpthw/ex21.py
@@ -0,0 +1,35 @@
+'''
+	Functions Can Return Something
+'''
+
+def add(a, b):
+	print(f"ADDING {a} + {b}")
+	return a + b
+
+def subtract(a, b):
+	print(f"SUBTRACTING {a} - {b}")
+	return a - b
+
+def multiply(a, b):
+	print(f"MULTIPLYING {a} * {b}")
+	return a * b
+
+def divide(a, b):
+	print(f"DIVIDING {a} / {b}")
+	return a / b
+
+print("Let's do some math with just functions!")
+
+age = add(20, 7)
+height = subtract(180, 8)
+weight = multiply(135, 5)
+iq = divide(200, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
