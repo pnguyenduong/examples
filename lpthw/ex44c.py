a9918bc4ca61ca35680b59e09cb51ff6faa6573c
Phong Nguyen <pnguyenduong@gmail.com>
2019-02-14 08:48:44 -0600

    Exercise 44

---
+++ lpthw/ex44c.py
@@ -0,0 +1,21 @@
+'''
+	Alter Before or After
+'''
+
+class Parent(object):
+
+	def altered(self):
+		print("PARENT altered()")
+
+class Child(Parent):
+
+	def altered(self):
+		print("CHILD, BORE PARENT altered()")
+		super(Child, self).altered()
+		print("CHILD, AFTER PARENT altered()")
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
